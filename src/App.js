4b728a7712bdd30837b61614725369e4cd343b0f
Robson Andrade <hagridd4@gmail.com>
2020-01-06 15:55:05 -0300

    Buscando produtos da api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/header';
 import './style.css';
+import Main from './pages/main';
 
 const App = () => (
   <div className="App">
   <Header />
+  <Main />
 </div>
 );
 
